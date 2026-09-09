81f94b450c7ce932507f03de86bb1a0652e3c3b7
Kamyar <kshahroudi97@gmail.com>
2026-09-09 10:58:28 +0330

    author command fixed

--- main_bot.py
+++ main_bot.py
@@ -110,20 +110,24 @@ Author = """
 
 I'm Kamyar Shahroudi, a software developer and tech enthusiast. I create projects focused on programming, automation, and networking while continuously learning and experimenting with new technologies.
 
-🔗 GitHub: https://github.com/kamyarshah-11
-💬 Telegram: https://t.me/KamyJooon
-📸 Instagram: https://instagram.com/kamyarr166
-📧 Email: kshahroudi97@gmail.com
 
 Thanks for using this bot! Your feedback and suggestions are always welcome.
 """
+Author_markup = InlineKeyboardMarkup()
+github_link_btn = InlineKeyboardButton("Git Hub",url="https://github.com/kamyarshah-11/") 
+telegram_link_btn = InlineKeyboardButton("Telegram",url="https://t.me/KamyJooon")
+email_btn = InlineKeyboardButton("Email",url="kshahroudi97@gmail.com")
+instagram_link_btn = InlineKeyboardButton("Instagram",url="https://instagram.com/kamyarr166")
+Author_markup.add(github_link_btn,telegram_link_btn)
+Author_markup.add(instagram_link_btn)
+Author_markup.add(email_btn)
 
 
 
 
-@bot.message_handler(commands=["Author"])
+@bot.message_handler(commands=["author"])
 def Author_info(message):
-    bot.send_message(message.chat.id, Author)
+    bot.send_message(message.chat.id, Author,reply_markup=Author_markup)
     return
 
 
